e62b7e6631ffac545d26235666109c8c7068d49a
Nimalan Kirubakaran <nimalankirubakaran@gmail.com>
2026-02-20 12:41:47 +0000

    Added kprobe
    Description:
    - kprobe fires when `execve` syscall is called

--- runtime_library/libplustwo.c
+++ runtime_library/libplustwo.c
@@ -2,14 +2,15 @@
 
 // This function runs automatically when the library is loaded
 void __attribute__((constructor)) init_library() {
-    printf("Running init_library function.\n");
+  printf("Running init_library function.\n");
 }
 
-// This function runs automatically when the library is unloaded (or program exits)
+// This function runs automatically when the library is unloaded (or program
+// exits)
 void __attribute__((destructor)) cleanup_library() {
-    printf("Running cleanup_library function.\n");
+  printf("Running cleanup_library function.\n");
 }
 
-char *get_library_name() { return "Add 2 to number"; }
+char* get_library_name() { return "I add 2 to ints"; }
 
 int do_operation(int i) { return i + 2; }
